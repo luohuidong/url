c50996e67d6b98dee230c63d7dbd8bf1b85ef9b4
luohuidong <luohuidong01@126.com>
2020-03-18 00:14:35 +0800

    Removed log.

--- src/parseUrl/index.test.ts
+++ src/parseUrl/index.test.ts
@@ -1,7 +1,6 @@
 import parse from './index'
 
 test('parse url', () => {
-  console.log('test', parse('https://www.test.com/a?b=2&c=3'))
   expect(parse('https://www.test.com/a?b=2&c=3')).toEqual({
     protocal: 'https:',
     search: '?b=2&c=3',
